12447d86f42147919fb00748e0ff58af135a2bb2
Pitt96 <roypalominor28.gmail.com>
2022-04-20 13:04:23 -0500

    Agrege validacion es y barras de carga

--- Atm_cajero.py
+++ Atm_cajero.py
@@ -1,5 +1,11 @@
 """Es un ejercicio basico en python
 Aautor:pitt"""
+import time
+from tqdm import tqdm
+print("\n              -------------------------------------------------------------INGRESANDO AL SISTEMA DE ATM-------------------------------------------------------------\n")
+for i in tqdm(range(100)):
+    time.sleep(0.1)
+
 saldo=1000.0
 ciclo=True
 aux=1
@@ -7,31 +13,57 @@ contra="123456789"
 reporte=[]
 reporte_soles=[]
 while ciclo:
-    print("\n********************MENU********************")
-    print("1 ---> Depositar en la cuenta")
-    print("2 ---> Retirar dinero de la cuenta")
-    print("3 ---> Visualizar dinero disponible")
-    print("4 ---> Cambiar la contraseña")
-    print("5 ---> Reporte de la cuenta")
-    print("6 ---> Salir")
-    opcion=int(input("Digite la opcion a realizar\n>"))
-
+    while aux==1:
+        try:
+            print("\n********************MENU********************")
+            print("1 ---> Depositar en la cuenta")
+            print("2 ---> Retirar dinero de la cuenta")
+            print("3 ---> Visualizar dinero disponible")
+            print("4 ---> Cambiar la contraseña")
+            print("5 ---> Reporte de la cuenta")
+            print("6 ---> Salir")
+            opcion=int(input("Digite la opcion a realizar\n>"))
+            aux=0
+        except ValueError:
+            print("\n---------------------------------")
+            print("            OPCION")
+            print("           INVALIDA")
+            print("        (SOLO NUMEROS)")
+            print("---------------------------------")
+    aux=1
     if opcion==1:
-        print("OPCION 1:-----------------------------------")
-        dinner_extra=float(input("Digite el dinero a depositar\nS/."))
-        saldo+=dinner_extra
-        reporte.append("Deposito")
-        reporte_soles.append(dinner_extra)
-        print("       ----------------    ")
-        print("         Deposito de    ")
-        print("         S/.",dinner_extra)
-        print("          EXITOSO    ")
-        print("       ----------------    ")
-
+        while aux==1:
+            try:
+                print("OPCION 1:-----------------------------------")
+                dinner_extra=float(input("Digite el dinero a depositar\nS/."))
+                if dinner_extra>0:
+                    saldo+=dinner_extra
+                    reporte.append("Deposito")
+                    reporte_soles.append(dinner_extra)
+                    print("       ----------------    ")
+                    print("         Deposito de    ")
+                    print("         S/.",dinner_extra)
+                    print("          EXITOSO    ")
+                    print("       ----------------    ")
+                    aux=0
+                else:
+                    print("\n---------------------------------")
+                    print("            INGRESO")
+                    print("            INVALIDO")
+                    print(" (El monto debe ser mayor que 0)")
+                    print("---------------------------------") 
+            except ValueError:
+                print("\n---------------------------------")
+                print("            OPCION")
+                print("           INVALIDA")
+                print("        (SOLO NUMEROS)")
+                print("---------------------------------")
+        aux=1
     elif opcion==2:
         print("OPCION 2:-----------------------------------")
         while aux==1:
-            aux_contra=input("Digite la CONTRASEÑA para retirar\n>")
+            aux_contra=input("Digite la 'CONTRASEÑA' para retirar\n>")
+
             if aux_contra== contra:
                 aux=0
             else:
@@ -39,23 +71,42 @@ while ciclo:
                 print("         Contraseña incorrecta         ")
                 print("       -------------------------    ")
         aux=1
+        print("\n              -------------------------------------------------------------INGRESANDO A LA CUENTA-------------------------------------------------------------\n")
+        for i in tqdm(range(100)):
+            time.sleep(0.05)
+
+        print("               --------------------------------------------------------------------------------------------------------------------------------------------------\n")
         while aux==1:
-            monto_retiro=float(input("Digite el monto a retirar\nS/."))
-            if monto_retiro>saldo:
-                print("       ------------------------------    ")
-                print("         No tienes saldo suficiente")
-                print("           para retirar ese monto.")
-                print("       ------------------------------    ")
-            else:
-                saldo=saldo-monto_retiro
-                reporte.append("Retiro")
-                reporte_soles.append(monto_retiro)
-                print("       ----------------    ")
-                print("         Retiro de    ")
-                print("         S/.",monto_retiro)
-                print("          EXITOSO    ")
-                print("       ----------------    ")
-                aux=0
+            try:
+                monto_retiro=float(input("Digite el monto a retirar\nS/."))
+                if monto_retiro>0:
+                    if monto_retiro>saldo:
+                        print("       ------------------------------    ")
+                        print("         No tienes saldo suficiente")
+                        print("           para retirar ese monto.")
+                        print("       ------------------------------    ")
+                    else:
+                        saldo=saldo-monto_retiro
+                        reporte.append("Retiro")
+                        reporte_soles.append(monto_retiro)
+                        print("       ----------------    ")
+                        print("         Retiro de    ")
+                        print("         S/.",monto_retiro)
+                        print("          EXITOSO    ")
+                        print("       ----------------    ")
+                    aux=0
+                else:
+                    print("\n---------------------------------")
+                    print("            INGRESO")
+                    print("            INVALIDO")
+                    print(" (El monto debe ser mayor que 0)")
+                    print("---------------------------------")
+            except ValueError:
+                print("\n---------------------------------")
+                print("            OPCION")
+                print("           INVALIDA")
+                print("        (SOLO NUMEROS)")
+                print("---------------------------------")
         aux=1
     elif opcion==3:
         print("OPCION 3:-----------------------------------")
@@ -94,7 +145,12 @@ while ciclo:
         print("              VUELVE PRONTO")
         print("--------------------------------------------")
         ciclo=False
-
+    else:
+        print("\n---------------------------------")
+        print("            OPCION")
+        print("           INVALIDA")
+        print("             [1-6]")
+        print("---------------------------------")
     
 
 
